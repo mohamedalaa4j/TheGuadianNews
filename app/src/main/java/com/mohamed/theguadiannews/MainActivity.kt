1ab9baf17c04fa5b7271823d791153df6ff7634e
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-05-21 17:06:19 +0200

    Init commit

---
+++ app/src/main/java/com/mohamed/theguadiannews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mohamed.theguadiannews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohamed.theguadiannews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+    }
+}
