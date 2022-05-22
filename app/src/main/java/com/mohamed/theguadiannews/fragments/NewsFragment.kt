d53219579176a32352ebc6a8b063a114f3d7edff
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-05-23 01:13:46 +0200

    SearchView done with limitation 50 results

--- app/src/main/java/com/mohamed/theguadiannews/fragments/NewsFragment.kt
+++ app/src/main/java/com/mohamed/theguadiannews/fragments/NewsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,13 +29,39 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
 
     var page = 1
     var totalPages = 1
+
     var isLoading = false
     val adapter = MyAdapter(dataList)
+    var searchKeyword = ""
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNewsBinding.bind(view)
+
+
+        //region SearchView Listener
+        binding?.toolBar?.searchBar?.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                binding?.toolBar?.searchBar?.clearFocus()
+               // Toast.makeText(context, binding?.toolBar?.searchBar?.query, Toast.LENGTH_SHORT).show()
+                searchKeyword = binding?.toolBar?.searchBar?.query.toString()
+                Log.i("search", searchKeyword)
+                apiCallSearch()
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                searchKeyword = binding?.toolBar?.searchBar?.query.toString()
+                Log.i("search", searchKeyword)
+                apiCallSearch()
+                return false
+
+            }
+        })
+        //endregion
 
         apiCall()
 
@@ -48,21 +75,23 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
         binding?.rv?.adapter = adapter
 
         /////region Endless RV code
-        binding?.rv?.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+        binding?.rv?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy > 0){
+                if (dy > 0) {
                     val visibleItemCount = linearLayoutManager.childCount
                     val pastVisibleItem = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
                     val total = adapter.itemCount
 
-                    if (!isLoading){
+                    if (!isLoading) {
 
-                        if ((visibleItemCount + pastVisibleItem) >= total){
+                        if ((visibleItemCount + pastVisibleItem) >= total) {
                             //Toast.makeText(context, "end", Toast.LENGTH_SHORT).show()
 
                             if (totalPages > page) {
-                                page++
-                            }else{isLoading = true}
+                              page++
+                            } else {
+                                isLoading = true
+                            }
                             isLoading = true
                             apiCall()
                             isLoading = false
@@ -74,13 +103,13 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
         })
         //endregion
         ///// Listener
-        adapter.setOnItemClickListener(object : MyAdapter.OnItemClickListener{
+        adapter.setOnItemClickListener(object : MyAdapter.OnItemClickListener {
             override fun onItemClick(position: Int) {
-                val newsUrl : String = dataList[position].webUrl
-                //Toast.makeText(context, newsUrl,Toast.LENGTH_SHORT).show()
+                val newsUrl: String = dataList[position].webUrl
 
+                // Url opener
                 val intent = Intent(context, UrlOpener::class.java)
-                intent.putExtra("url",newsUrl)
+                intent.putExtra("url", newsUrl)
                 startActivity(intent)
 
 
@@ -113,8 +142,33 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
 
     }
 
+    private fun apiCallSearch() {
+
+        val queue = Volley.newRequestQueue(context)
+
+        val url =
+            "https://content.guardianapis.com/search?q=$searchKeyword&show-fields=thumbnail&order-by=newest&page-size=50&api-key=43f906b3-25e9-4762-9233-29811f58038b"
+        Log.i("link",url)
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+
+                parse(response.toString())
+
+            },
+            { error ->
+                Log.e("api", error.toString())
+                Toast.makeText(context, "Error getting the data!", Toast.LENGTH_LONG).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
+
+    }
+
+
     private fun parse(data: String) {
 
+        dataList.clear()
 
         val jsonObject = JSONTokener(data).nextValue() as JSONObject
         val responseObject = jsonObject.getString("response")
@@ -149,61 +203,23 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
             } catch (e: Exception) {
             }
 
-            dataList.add(Model(webTitle, date, thumbnail,webUrl))
+
+            dataList.add(Model(webTitle, date, thumbnail, webUrl))
 
             adapter.notifyDataSetChanged()
-           // rvSetup()
+            // rvSetup()
 
         }
     }
 
-//    private fun rvSetup() {
-//
-//
-//        // Set the LayoutManager that this RecyclerView will use.
-//        val linearLayoutManager = LinearLayoutManager(context)
-//        binding?.rv?.layoutManager = linearLayoutManager
-//        // adapter instance is set to the recyclerview to inflate the items.
-//        val adapter = MyAdapter(dataList)
-//        binding?.rv?.adapter = adapter
-//
-//        /////////
-//        binding?.rv?.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-//                if (dy > 0){
-//                    val visibleItemCount = linearLayoutManager.childCount
-//                    val pastVisibleItem = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
-//                    val total = adapter.itemCount
-//
-//                    if (!isLoading){
-//
-//                        if ((visibleItemCount + pastVisibleItem) >= total){
-//                            Toast.makeText(context, "end", Toast.LENGTH_SHORT).show()
-//
-//                            if (totalPages > page) {
-//                                page++
-//                            }else{isLoading = true}
-//                            isLoading = true
-//                            apiCall()
-//                            isLoading = false
-//                        }
-//                    }
-//                }
-//                super.onScrolled(recyclerView, dx, dy)
-//            }
-//        })
-//        /////////
-//
-//
-//    }
 
     private fun parseDate(date: String): String {
 
         val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
-       val mDate = formatter.parse(date)
-      //  Log.i("datep", mDate.toString())
+        val mDate = formatter.parse(date)
         return mDate!!.toString().take(10)
 
     }
+
 
 }
